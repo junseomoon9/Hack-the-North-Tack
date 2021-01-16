8025258f39e46205f8346a406a14741e71c31502
1moonala <junseomoon9@gmail.com>
2021-01-16 13:16:06 -0500

    pushed login.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,11 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
+      
         <Route path='/' exact />
+        <Route path='/login'>
+          <Login/>
+        </Route>
         <Route path='/map'>
           <Map/>
         </Route>
---
+++ client/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const login = () => {
+    return (
+        <div className="">
+            
+        </div>
+    )
+}
