7859dad01876f7df09e123fdeae8183ac6dc8ba8
Jillian Exconde <jillianexconde@Jillians-MacBook-Pro.local>
2021-01-16 15:20:10 -0500

    login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './Login.css'
 
 export const Login = () => {
     return (
-        <div className="">
-            
+        <div className="welcome">
+            <h1> Welcome to Tack!</h1>
+            <label>
+                 <p>Email</p>
+                <input type="text" />
+            </label>
+            <label>
+                 <p>Password</p>
+                <input type="password" />
+             </label>
+            <div>
+                <button type="submit">Submit</button>
+             </div>
         </div>
+
+        
     )
 }
