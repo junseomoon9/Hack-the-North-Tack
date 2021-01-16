fd016a54458e16df2f680f6a52da1f1b78252cb4
kensonhui <asimplekevin@gmail.com>
2021-01-16 10:03:50 -0500

    Added login, signup, landing pages
    Currently still need to add id for each element
    Need to add in text fields
    Need to rename class names

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Landing from './components/landing.js';
+import Login from './components/login.js';
+import Signup from './components/signup.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path='/' component={Landing} exact />
+      <Route path='/signup' component={Signup} />
+      <Route path='/login' component={Login} />
+      <Route component={Error}/>
+    </Switch>
   );
 }
 
